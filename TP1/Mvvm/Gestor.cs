dcfdc30d506fc2e38db1b7c5ebabeea8346f11c5
damian-ed <demian.robocop@gmail.com>
2022-08-15 13:45:35 -0300

    probabilidaddes en intervalos

--- TP1/Mvvm/Gestor.cs
+++ TP1/Mvvm/Gestor.cs
@@ -73,5 +73,70 @@ namespace TP1.Mvvm
             }
 
         }
+
+        public static List<decimal> probabilidad(List<decimal> numeros_aleatorios)
+        {
+            List<decimal> array= new List<decimal>(); //aca se crea unna nueva lista donde contendra los valores de las probabilidades de cada intervalo
+            int simActual = 1;
+            int simAnterior = 0;
+            decimal p1 = 0;
+            decimal p2 = 0;
+            decimal p3 = 0;
+            decimal p4 = 0;
+            decimal p5 = 0;
+            decimal p6 = 0;
+            decimal p7 = 0;
+            decimal p8 = 0;
+            decimal p9 = 0;
+            decimal p10 = 0;
+            foreach (decimal i in numeros_aleatorios)
+            {
+
+                if (i * 10000 < 1000) { p1 = (p1 * simAnterior + 1) / simActual; }                    // no me dejaba comparar decimales asi que los paso a entero y los comparo
+                else { p1 = (p1 * simAnterior + 0) / simActual; }
+
+                if (1000 <= i * 10000 & i * 10000 < 2000) { p2 = (p2 * simAnterior + 1) / simActual; }
+                else { p2 = (p2 * simAnterior + 0) / simActual; }
+
+                if (2000 <= i * 10000 & i * 10000 < 3000) { p3 = (p3 * simAnterior + 1) / simActual; }
+                else { p3 = (p3 * simAnterior + 0) / simActual; }
+
+                if (3000 <= i * 10000 & i * 10000 < 4000) { p4 = (p4 * simAnterior + 1) / simActual; }
+                else { p4 = (p4 * simAnterior + 0) / simActual; }
+
+                if (4000 <= i * 10000 & i * 10000 < 5000) { p5 = (p5 * simAnterior + 1) / simActual; }
+                else { p5 = (p5 * simAnterior + 0) / simActual; }
+
+                if (5000 <= i * 10000 & i * 10000 < 6000) { p6 = (p6 * simAnterior + 1) / simActual; }
+                else { p6 = (p6 * simAnterior + 0) / simActual; }
+
+                if (6000 <= i * 10000 & i * 10000 < 7000) { p7 = (p7 * simAnterior + 1) / simActual; }
+                else { p7 = (p7 * simAnterior + 0) / simActual; }
+
+                if (7000 <= i * 10000 & i * 10000 < 8000) { p8 = (p8 * simAnterior + 1) / simActual; }
+                else { p8 = (p8 * simAnterior + 0) / simActual; }
+
+                if (8000 <= i * 10000 & i * 10000 < 9000) { p9 = (p9 * simAnterior + 1) / simActual; }
+                else { p9 = (p9 * simAnterior + 0) / simActual; }
+
+                if (9000 <= i * 10000 & i * 10000 < 10000) { p10 = (p10 * simAnterior + 1) / simActual; }
+                else { p10 = (p10 * simAnterior + 0) / simActual; }
+                simAnterior = simActual;
+                simActual++;
+
+            }
+            array.Add(p1); // va agregando al fnal del forech el reultado de cada probabiliddad seguro hay una forma de hacerlo en 1 linea
+            array.Add(p2);
+            array.Add(p3);
+            array.Add(p4);
+            array.Add(p5);
+            array.Add(p6);
+            array.Add(p7);
+            array.Add(p8);
+            array.Add(p9);
+            array.Add(p10);
+
+            return array; // esto habria que mostrar u de ultima para cada pN dentro de un label entonces el return seria cada probabilidad y no una lista
+        }
     }
 }
